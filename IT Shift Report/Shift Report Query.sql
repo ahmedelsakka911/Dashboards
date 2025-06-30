c4a30f0f1ce051261b39a9a5e72fb56be3dbf38e
ahmedelsakka911 <87879217+ahmedelsakka911@users.noreply.github.com>
2025-06-30 13:49:10 +0300

    Add files via upload

---
+++ IT Shift Report/Shift Report Query.sql
@@ -0,0 +1,58 @@
+SELECT 
+    "wo"."WORKORDERID" AS "Request ID",
+    "std"."STATUSNAME" AS "Status",
+    "mdd"."MODENAME" AS "Request Mode", 
+    "ti"."FIRST_NAME" AS "Technician", 
+    "cd"."CATEGORYNAME" AS "Category", 
+    "cri"."FIRST_NAME" AS "Created By", 
+    "dpt"."DEPTNAME" AS "Department", 
+    "wotodesc"."FULLDESCRIPTION" AS "Description", 
+    "qd"."QUEUENAME" AS "Group", 
+    "aau"."FIRST_NAME" AS "Requester", 
+    "wo"."TITLE" AS "Subject", 
+    "wos"."ASSIGNEDTIME" AS "Assigned Time", 
+    "serdef"."NAME" AS "Service Category", 
+    "wo"."CREATEDTIME" AS "Created Time",
+    "wo"."COMPLETEDTIME" AS "Completed Time",
+    "raf1"."UDF_DATE1" AS "From", 
+    "raf1"."UDF_DATE2" AS "To" ,
+    "Request_UDF_PickListValues_UDF_PICK7"."VALUE" AS "Planned(Y/N)"
+
+FROM "WorkOrder" "wo"
+LEFT JOIN "ModeDefinition" "mdd" ON "wo"."MODEID" = "mdd"."MODEID"
+LEFT JOIN "SDUser" "sdu" ON "wo"."REQUESTERID" = "sdu"."USERID"
+LEFT JOIN "AaaUser" "aau" ON "sdu"."USERID" = "aau"."USER_ID"
+LEFT JOIN "DepartmentDefinition" "dpt" ON "wo"."DEPTID" = "dpt"."DEPTID"
+LEFT JOIN "WorkOrderStates" "wos" ON "wo"."WORKORDERID" = "wos"."WORKORDERID"
+LEFT JOIN "CategoryDefinition" "cd" ON "wos"."CATEGORYID" = "cd"."CATEGORYID"
+LEFT JOIN "SDUser" "td" ON "wos"."OWNERID" = "td"."USERID"
+LEFT JOIN "AaaUser" "ti" ON "td"."USERID" = "ti"."USER_ID"
+LEFT JOIN "StatusDefinition" "std" ON "wos"."STATUSID" = "std"."STATUSID"
+LEFT JOIN "WorkOrder_Queue" "woq" ON "wo"."WORKORDERID" = "woq"."WORKORDERID"
+LEFT JOIN "Request_Additional_Fields_1" "raf1" ON "wo"."WORKORDERID"="raf1"."WORKORDERID"
+LEFT JOIN "QueueDefinition" "qd" ON "woq"."QUEUEID" = "qd"."QUEUEID"
+LEFT JOIN "SDUser" "crd" ON "wo"."CREATEDBYID" = "crd"."USERID"
+LEFT JOIN "AaaUser" "cri" ON "crd"."USERID" = "cri"."USER_ID"
+LEFT JOIN "WorkOrderToDescription" "wotodesc" ON "wo"."WORKORDERID" = "wotodesc"."WORKORDERID"
+LEFT JOIN "Request_Additional_Fields_2" "raf2" ON "wo"."WORKORDERID"="raf2"."WORKORDERID" 
+LEFT JOIN "Request_UDF_PickListValues" "Request_UDF_PickListValues_UDF_PICK7" ON "raf2"."UDF_PICK7"="Request_UDF_PickListValues_UDF_PICK7"."PickListID" 
+LEFT JOIN "ServiceDefinition" "serdef" ON "wo"."SERVICEID" = "serdef"."SERVICEID"
+WHERE 
+    
+	(
+        (
+            "qd"."QUEUENAME" = N'IFS' OR "qd"."QUEUENAME" = N'IFS Group' OR "qd"."QUEUENAME" =N'Information Security' OR "qd"."QUEUENAME"=N'Infrastructure' OR "qd"."QUEUENAME"=N'Infrastructure Group' OR "qd"."QUEUENAME" = N'IT-DB' OR "qd"."QUEUENAME"=N'IT-Development' OR "qd"."QUEUENAME"=N'IT-Support' OR "qd"."QUEUENAME"=N'IT-Systems' OR "qd"."QUEUENAME" = N'IT-Systems Group'OR "qd"."QUEUENAME" = N'IT-TOS'OR "qd"."QUEUENAME" = N'Mobile Service'OR "qd"."QUEUENAME" = N'Pending Accounts'OR "qd"."QUEUENAME" = N'Physical Security'OR "qd"."QUEUENAME" = N'Printing Service'OR "qd"."QUEUENAME" = N'SCCT- IT Staff'
+        ) 
+        AND 
+        (
+            (
+                "wo"."CREATEDTIME" >= 1635689600000 AND  -- Start of 2024 (2024-01-01 00:00:00)
+                "wo"."CREATEDTIME" <= 1930297599000 AND  -- End of 2027 (2027-12-31 23:59:59)
+                "wo"."CREATEDTIME" != 0 AND 
+                "wo"."CREATEDTIME" IS NOT NULL AND 
+                "wo"."CREATEDTIME" != -1
+            )
+        )
+    ) 
+    AND "wo"."ISPARENT" = '1'
+	;
